472ac056a99beabf5f00a15a5a2d4bd1db01c4fe
Gabriel Ohowa Owino <s1871518@Asperger17-2.local>
2019-04-02 20:14:30 +0100

    kill other node instances before deployment

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.get('/', function(req, res){
   res.send("hello world");
 })
 
-app.post('/', function(req, res){
-  console.log(req);
-})
+// app.post('/', function(req, res){
+//   res.send(req);
+// })
 
 app.listen(port, function(){
   console.log("app running on port " + port);
