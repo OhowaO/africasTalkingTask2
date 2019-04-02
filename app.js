c41bcf6db3b2f20ee003cd35f4f15f464b2317fb
Gabriel Ohowa Owino <s1871518@Asperger17-2.local>
2019-04-02 19:55:33 +0100

    server skeleton and first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+
+app.get('/', function(req, res){
+  res.send("hello world");
+})
+
+app.post('/', function(req, res){
+  console.log(req);
+})
+
+app.listen(port, function(){
+  console.log("app running on port " + port);
+})
