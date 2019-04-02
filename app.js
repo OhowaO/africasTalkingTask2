8c7b3c8937facc1ba802a8a991a50e251a5973a2
Gabriel Ohowa Owino <s1871518@Asperger17-2.local>
2019-04-02 20:19:26 +0100

    port change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
   res.send("hello world");
