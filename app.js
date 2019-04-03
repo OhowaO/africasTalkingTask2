68f8baebee1a693a42ad8a40d5201064a8da5d75
Gabriel Ohowa Owino <s1871518@Asperger17-2.local>
2019-04-03 01:32:33 +0100

    trying req json -> string

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-
 app.get('/', function(req, res){
   res.send("hello world");
 })
@@ -11,7 +9,7 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
   //console.log(req);
   console.log('Callback noted');
-  console.log('[BODY]' + req.body.sessionId);
+  console.log('[BODY]' + JSON.stringify(req.body));
   res.send('CON menu is here');
 })
 
