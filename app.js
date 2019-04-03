ae6cdeae7851aff6d4a7743429a7fa5f50dcc268
Gabriel Ohowa Owino <s1871518@Asperger17-2.local>
2019-04-03 01:35:19 +0100

    trying req json -> string

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
   //console.log(req);
   console.log('Callback noted');
-  console.log('[BODY]' + JSON.stringify(req));
+  console.log(req);
   res.send('CON menu is here');
 })
 
